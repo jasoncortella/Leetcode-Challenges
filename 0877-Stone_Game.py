62787b4fae6a8ca929ffb31eaefd0ad5c567251f
Jason Cortella <Jason@Jasons-MacBook-Air.local>
2019-10-08 11:47:52 -0700

    Improve documentation

--- 0877-Stone_Game.py
+++ 0877-Stone_Game.py
@@ -1,3 +1,5 @@
+# Note - 'return True' passes the check, because the problem mandates the number of piles will always be even. If this constraint were not in palce, the below code would work for all cases
+
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         a = 0
